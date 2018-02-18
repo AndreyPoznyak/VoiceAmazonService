232e7237d8c87fe6c706d499551daec48d425daa
Andrey Poznyak <Andrey.Poznyak@skywindgroup.com>
2018-02-18 23:27:43 +0300

    fixed db yml description

--- handler.js
+++ handler.js
@@ -8,18 +8,33 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'RootQueryType' // an arbitrary name
-    }),
-});
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
-    .then(result => callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(result)
-        }), error => callback(error)
-    );
+const promisify = foo => new Promise((resolve, reject) => {
+    foo((error, result) => {
+        if(error) {
+            reject(error)
+        } else {
+            resolve(result)
+        }
+    })
+})
+
+// const schema = new GraphQLSchema({
+//     query: new GraphQLObjectType({
+//         name: 'RootQueryType' // an arbitrary name
+//     }),
+// });
+
+module.exports.query = (event, context, callback) => {
+    
+
+    callback(null, {
+        statusCode: 200,
+        body: "Hello"
+    });
+};
 
 module.exports.hello = (event, context, callback) => {
     console.log(event); // Contains incoming request data (e.g., query params, headers and more)
