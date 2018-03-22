abe6b261d115d28ff9c37e0ac5cc7e9d08623a54
Andrey Poznyak <Andrey.Poznyak@skywindgroup.com>
2018-03-22 19:12:53 +0300

    parsed yml + added basic server

---
+++ debug/server.js
@@ -0,0 +1,28 @@
+const YAML = require('yamljs');
+const express = require('express');
+
+const app = express();
+
+const slsConfig = YAML.load('serverless.yml');
+
+Object.values(slsConfig.functions).forEach(func => {
+    let description = func.events[0].http;
+
+    if (description.method === "post") {
+        app.post(description.path, (req, res) => {
+            //res.send('Hello World!');
+        });
+    } else if (description.method === "get") {
+        app.get(description.path, (req, res) => {
+            //res.send('Hello World!');
+        });
+    } else {
+        console.log("Unsupported http method detected");
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+//app.listen(3000, () => console.log('Test server listening on port 3000!'));
